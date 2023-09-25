080d26287ed1d9e64474d9e30d472bc5c2652fc2
Asier Fernandez <asierfc@gmail.com>
2023-09-25 12:11:28 +0200

    Class PotionBag created

--- potionBag.mjs
+++ potionBag.mjs
@@ -1,10 +1,25 @@
+export { PotionBag } 
 
+import { Cauldron } from "./cauldron.mjs"
 
 class PotionBag{
 
     constructor (listOfPotions){
-        
+        this.listOfPotions = listOfPotions
     }
 
+    createPotions(ingredients, cauldron){
+
+        const potionBag = [];
+
+        for(let i = 0; i < ingredients.length; i++){
+            for( let j = i+1; j <= ingredients.length; j++){
+                potionBag.push(cauldron.createPotion(ingredients[i], ingredients[j]));
+            }
+        }
+
+        return new PotionBag(potionBag);
+        
+    }
 
 }
